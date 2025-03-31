25080f18c5ef780835c9f621fc7448da9dc372b5
doncici77 <pkh2956@naver.com>
2025-03-31 16:54:19 +0900

    재귀 함수 기본과 언어의 기본 다지기

---
+++ SandBox_Console/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace SandBox_Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                for(int j = 0; j < i + 1; j++)
+                {
+                    Console.Write('*');
+                }
+                Console.WriteLine();
+            }
+
+            A(1);
+        }
+
+        static void A(int num)
+        {
+            // 기저 조건
+            if(num > 5)
+            {
+                return;
+            }
+
+            // 반복 구문
+            B(num);
+            if(num < 5)
+            {
+                Console.WriteLine();
+            }
+            num++;
+
+            // 재귀 조건
+            A(num);
+        }
+
+        static void B(int num)
+        {
+            if(1 > num)
+            {
+                return;
+            }
+
+            Console.Write('+');
+            num--;
+
+            B(num);
+        }
+    }
+}
