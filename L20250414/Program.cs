ba5a1c7eba44bf9c3960ab3e8703060a823b5bda
doncici77 <pkh2956@naver.com>
2025-04-14 14:08:57 +0900

    스택에 대하여

---
+++ L20250414/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace L20250414
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> st = new();
+
+            st.Push(1);
+            st.Push(2);
+            st.Push(3);
+
+            st.Peek();
+
+            int num = st.Pop();
+            num = st.Pop();
+            num = st.Pop();
+        }
+    }
+}
