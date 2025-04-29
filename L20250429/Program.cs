d85e17e3e0c7b09f9a7d70c3893c434f8c5ea697
doncici77 <pkh2956@naver.com>
2025-04-29 10:43:06 +0900

    init

---
+++ L20250429/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace L20250429
+{
+    internal class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
